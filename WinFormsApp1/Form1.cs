1eddae4ee05e1dbe4319f6e1f35a20fc7f2696bf
Syxsaran <96071669+Syxsaran@users.noreply.github.com>
2023-11-17 15:23:40 +0700

    Initial commit

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
